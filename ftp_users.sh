8c3020ea0e8c9100ace6503fcc0b83546efd850e
deadsen <deadsen@localtest.dream.net.ua>
2019-10-29 15:17:47 +0200

    Revison 1.0

--- ftp_users.sh
+++ ftp_users.sh
@@ -9,7 +9,6 @@ echo -e "1 - Create user\n\
 4 - Show users \n\
 5 - Edit disk quotas\n\
 0 - Exit"
-#trap "echo ' Test'" EXIT
 read -p "Make your choise: " menu
 #########CHECK INPUT VALUES#########
 	if ! [[ $menu =~ $re ]];then
@@ -31,18 +30,17 @@ read -p "Make your choise: " menu
 			pass=$(pwgen -y 16 | awk '{print $1}')
 			useradd -d /var/ftp/$user -s /sbin/nologin $user
 			echo "$pass" | passwd --stdin $user
-			#echo -e "-----------------------------------\nYour password is:$pass\n-----------------------------------"
 ##########ADD QUOTA#####
 			read -p "Whoud you like to add a user disk quota?(As a default there is no quota) y/n:" varquota
 			if [[ $varquota == Y || $varquota == y ]];then
 				#echo "yes"#add quota code should be there
 				read -p 'Please enter number of gigabytes for userquota : ' quota
 				setquota $user $quota'G' $quota'G' 0 0 /var/ftp
-				echo -e "-----------------------------------\nYour password is:$pass\n-----------------------------------"
+				echo -e "-----------------------------------\nYour password is:$pass \n-----------------------------------"
 				#echo "$(repquota -a | grep $user)"
 			else
 				echo "Warning!!! There is no quota as a default"
-				echo -e "-----------------------------------\nYour password is:$pass\n-----------------------------------"
+				echo -e "-----------------------------------\nYour password is:$pass \n-----------------------------------"
 			fi
 		else
 			echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nUsername can not be empty\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -53,7 +51,7 @@ read -p "Make your choise: " menu
 	elif [ $menu -eq 2 ];then 
 		echo "Changing password"
 		read -p 'Enter Username: ' user
-		if grep -w "$user" '/etc/passwd' >> /dev/null;then
+		if grep -w "$user" '/etc/passwd' | grep -w "/var/ftp" '/etc/passwd' >> /dev/null;then
 			pass=$(pwgen -y 16 | awk '{print $1}')
 			echo "$pass" | passwd --stdin $user
 			echo -e "-----------------------------------\nYour password is:$pass\n-----------------------------------"
@@ -67,7 +65,7 @@ read -p "Make your choise: " menu
 	elif [ $menu -eq 3 ];then
 		echo "Deleting user"
 		read -p 'Enter Username: ' user
-		if grep -w "$user" '/etc/passwd' >> /dev/null;then
+		if grep -w "$user" '/etc/passwd' | grep -w "/var/ftp" '/etc/passwd' >> /dev/null;then
 			read -p 'Would you like, to delete the home directory y/n (NO is default): ' path
 			read -p "Are you really would like to delete $user y/n: " del
 			while [ true ]
@@ -120,15 +118,19 @@ read -p "Make your choise: " menu
 				repquota -a
 			elif [ $check -eq 2 ];then
 				read -p 'Username : ' user
-				read -p 'Please enter number gigabytes for userquota (Numbers only) : ' quota
-				if [[ $quota =~ $re ]];then
-					setquota $user $quota'G' $quota'G' 0 0 /var/ftp
-					echo -e '-----------------------------------\nQuota successfully updated\n-----------------------------------'
-					echo '----------------------------------------------------------------------'
-					repquota -a | grep $user
-					echo '----------------------------------------------------------------------'
+				if grep -w "$user" '/etc/passwd' >> /dev/null;then
+					read -p 'Please enter number gigabytes for userquota (Numbers only) : ' quota
+					if [[ $quota =~ $re ]];then
+						setquota $user $quota'G' $quota'G' 0 0 /var/ftp
+						echo -e "-----------------------------------\nQuota successfully updated\n-----------------------------------"
+						echo '----------------------------------------------------------------------'
+						repquota -a | grep $user
+						echo '----------------------------------------------------------------------'
+					else
+						echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: You can only enter number values\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+					fi
 				else
-					echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nError: You can only enter number values\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                			echo -e "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nUser $user does not exist\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 				fi
 			elif [ $check -eq 0 ];then
 			break
